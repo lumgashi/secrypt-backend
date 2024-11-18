db60e65ad87934f9dca227c643db699b1395c1c9
lumgashi <lumgashi97@hotmail.com>
2024-11-18 14:54:00 +0100

    add more file types

--- src/utils/mimetypes.ts
+++ src/utils/mimetypes.ts
@@ -1,16 +1,20 @@
 export enum MIME_TYPES {
   DOC = 'application/msword',
   DOCX = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-  JPG = 'image/jpeg',
+  JPG = 'image/jpg',
+  JPEG = 'image/jpeg',
   PNG = 'image/png',
   GIF = 'image/gif',
   ZIP = 'application/zip',
   RAR = 'application/x-rar-compressed',
   PDF = 'application/pdf',
   MP4 = 'video/mp4',
+  MP3 = 'audio/mpeg',
   MOV = 'video/quicktime',
   AVI = 'video/x-msvideo',
   MKV = 'video/x-matroska',
+  PPTX = 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+  XLSX = 'application/vnd.ms-excel',
 }
 
 export const MIME_TYPES_ARRAY = Object.values(MIME_TYPES);
