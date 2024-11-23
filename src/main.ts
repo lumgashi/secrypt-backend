372bf3a7c398ee2f062d17c639a7ef5f0c048ea6
lumgashi <lumgashi97@hotmail.com>
2024-11-23 17:50:17 +0100

    increase json limit size

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,15 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 import { Logger } from 'nestjs-pino';
-import { urlencoded, json } from 'express';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(helmet());
-  app.use(json({ limit: '50mb' }));
-  app.use(urlencoded({ extended: true, limit: '50mb' }));
+  app.use(bodyParser.json({ limit: '2gb' }));
+  app.use(bodyParser.urlencoded({ limit: '2gb', extended: true }));
   app.useLogger(app.get(Logger));
   BigInt.prototype['toJSON'] = function () {
     const int = Number.parseInt(this.toString());
