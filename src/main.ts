312dd3a2b39419f27cb16b831b6896abbc189b15
lumgashi <lumgashi97@hotmail.com>
2024-11-23 16:22:44 +0100

    set new limit for json

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 import { Logger } from 'nestjs-pino';
+import { urlencoded, json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(helmet());
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   app.useLogger(app.get(Logger));
   BigInt.prototype['toJSON'] = function () {
     const int = Number.parseInt(this.toString());
